3ad1356ab2aefd57880924e6ac87e6a06dbfdbeb
DayanaBarquero <68917196+DayanaBarquero@users.noreply.github.com>
2020-08-31 21:24:34 -0600

    Add class IBanckTransations

---
+++ src/IBankTrasactions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dayan on 31/8/2020.
+//
+
+#include "IBankTrasactions.h"
---
+++ src/IBankTrasactions.h
@@ -0,0 +1,14 @@
+//
+// Created by dayan on 31/8/2020.
+//
+
+#ifndef MY_PROJECT_NAME_IBANKTRASACTIONS_H
+#define MY_PROJECT_NAME_IBANKTRASACTIONS_H
+
+
+class IBankTrasactions {
+
+};
+
+
+#endif //MY_PROJECT_NAME_IBANKTRASACTIONS_H
